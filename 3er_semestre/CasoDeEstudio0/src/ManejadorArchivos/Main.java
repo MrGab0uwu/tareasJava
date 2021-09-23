beab20981756383a4b47fffc5f0cf3700a062e3c
Gabriel <gabriel.escobar.torres@gmail.com>
2021-09-23 00:12:13 -0500

    cambios menores 2

--- 3er_semestre/CasoDeEstudio0/src/ManejadorArchivos/Main.java
+++ 3er_semestre/CasoDeEstudio0/src/ManejadorArchivos/Main.java
@@ -1,5 +1,5 @@
-
 package ManejadorArchivos;
+
 import FuncionesArchivo.*;
 
 /**
@@ -12,16 +12,26 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+
         
-        String[] contenido = {"4387","5812","9132","4584","8919"};
-        
+        // 0.1 Mostrar elementos de un arreglo de tipo texto
+        String[] contenido = {"Hola", "mundo", "desde", "Ubuntu", "20.04", "LTS"};
+        System.out.println("Elementos del arreglo contenido:");
+        for (String i : contenido) {
+            System.out.print(i+" ");
+        }
+
+        // 0.2 Mostrar las lineas de un archivo de texto
         var archivo = new CrearArchivo("arch.txt");
         String nombreArchivo = archivo.getNombreArchivo();
         var escritor = new EscribirArchivo(nombreArchivo);
         escritor.sobreEscribir(contenido);
         var leector = new LeerArchivo(nombreArchivo);
         leector.leer();
+
+        // 0.3 Mostrar los registros de un archivo binario, numero entero y un texto
         
+
     }
-    
+
 }
